d4d7b03887f5a469699220243ca6c63e3cf42dcd
minhalalikhan <minhalalikhan.786@gmail.com>
2024-01-05 13:09:00 +0530

    created Custom-Emitter using events module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 const express = require('express')
+const EventEmitter = require('events')
+
 const app = express()
 const PORT = 3000
 
-app.get("/", (req, res) => {
+const CustomEmitter = new EventEmitter()
+CustomEmitter.on('Init', (data) => console.log(`new Object Created of type ${data.constructor.name}`))
+CustomEmitter.on('processing', (data) => console.log(`Object is processing new data`))
+CustomEmitter.on('finish', (data) => console.log(`Process finished`))
 
-    res.send('Node Events App')
-})
+class DummyEmitter {
 
-app.listen(PORT, () => {
-    console.log("App is listening on PORT : " + PORT)
-})
+
+    constructor() {
+        // console.log(this)
+        this.name = 'this is a dummy emitter';
+        this.id = '536482'
+        CustomEmitter.emit('Init', this)
+    }
+
+    async process(a, b) {
+        CustomEmitter.emit('processing')
+
+        function delayit() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve()
+                }, 2000);
+            })
+        }
+
+        await delayit().then(() => console.log(a + b)).then(() => CustomEmitter.emit('finish'))
+
+
+
+    }
+}
+
+
+const d1 = new DummyEmitter()
+d1.process(30, 40)
